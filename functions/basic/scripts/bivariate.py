c495c45c2b554f5b6874f97742ae301692ec3439
ArataYanazume <a-yanazume@rikkyo.ac.jp>
2025-05-27 17:54:06 +0900

    Edit: bivariate analysis

--- functions/basic/scripts/bivariate.py
+++ functions/basic/scripts/bivariate.py
@@ -7,11 +7,11 @@ import numpy as np
 # Functions
 #----------------------------------------------------------------------#
 def covariance_matrix(
-        X: np.ndarray
+        data: np.ndarray
     ) -> np.ndarray:
     """ 分散共分散行列
     - numpy行列
     - バイアスあり（n-1）
     """
-    covariance = np.cov(X, rowvar=False, ddof=1)
+    covariance = np.cov(data, rowvar=False, ddof=1)
     return covariance
